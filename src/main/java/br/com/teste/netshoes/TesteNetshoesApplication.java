8f519c0e58e03ea19f4687646adfc90fd4cb0c47
Jackson Maia <jacksonmaia7@gmail.com>
2018-12-03 15:01:15 -0300

    Atualizando projeto 4

--- src/main/java/br/com/teste/netshoes/TesteNetshoesApplication.java
+++ src/main/java/br/com/teste/netshoes/TesteNetshoesApplication.java
@@ -2,11 +2,17 @@ package br.com.teste.netshoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TesteNetshoesApplication extends SpringBootServletInitializer{
-
+	
+	@Override 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TesteNetshoesApplication.class);
+    }
+	
     public static void main(String[] args) {
         SpringApplication.run(TesteNetshoesApplication.class, args);
     }
